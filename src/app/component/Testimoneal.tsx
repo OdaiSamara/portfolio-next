eca2561faf3bd9cfce9791e9aed5c3c4a5575ba6
OdaiSamara <166866058+OdaiSamara@users.noreply.github.com>
2024-09-22 15:12:25 -0700

    Update Testimoneal.tsx

--- src/app/component/Testimoneal.tsx
+++ src/app/component/Testimoneal.tsx
@@ -244,7 +244,7 @@ const Testimoneal = () => {
                         <span className="sr-only">Previous</span>
                     </span>
                 </button>
-                <button type="button" className="absolute -right-10 top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next onClick={moveleft} >
+                <button type="button" className="absolute -right-10 top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next  >
                     <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-slate-900 ">
                         <svg className="w-4 h-4 text-white " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
