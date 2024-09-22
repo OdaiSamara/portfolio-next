357139d7c27caf000503dcae1519a59c9a2573da
OdaiSamara <166866058+OdaiSamara@users.noreply.github.com>
2024-09-22 15:01:15 -0700

    Update Hero.tsx

--- src/app/component/Hero.tsx
+++ src/app/component/Hero.tsx
@@ -3,7 +3,7 @@ const Hero = () => {
     return (
         <div className='relative '>
               {/* @typescript-eslint/no-unused-vars */}
-            <div className='bg-sky-950  absolute -top-20  left-0 -z-55  w-full h-full '></div>
+            <div className='bg-sky-950  absolute -top-20  left-0 -z-55  w-full h-full '/ >
             <div
                 className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
                 <div className="  max-w-xl flex flex-col justify-center items-center ml-[50%]  ">
