a06d1700f46af4960bd45644a0d247b089d2e15b
OdaiSamara <166866058+OdaiSamara@users.noreply.github.com>
2024-09-22 14:47:34 -0700

    Update Hero.tsx

--- src/app/component/Hero.tsx
+++ src/app/component/Hero.tsx
@@ -1,23 +1,17 @@
-"use client"
 import React from 'react'
 const Hero = () => {
     return (
         <div className='relative '>
             <div className='bg-sky-950  absolute -top-20  left-0 -z-55  w-full h-full '></div>
-
             <div
-                className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8"
-            >
+                className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
                 <div className="  max-w-xl flex flex-col justify-center items-center ml-[50%]  ">
                     <h1 className="text-3xl  font-extrabold text-white sm:text-5xl">
                         Odai Samara
                     </h1>
-
                     <p className="typeone mt-4 text-white font-semibold sm:text-3xl/relaxed">
-                        I'am <span className='typetext '>Front-End!</span>
+                        I'am <span className='typetext '>Front-End</span>
                     </p>
-
-
                 </div>
             </div>
 
